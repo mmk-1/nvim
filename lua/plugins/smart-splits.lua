a907ae1ce4ead9a1e47ede5e4ddd02cc6d658329
Mahdi Khosravi <mmk1776@gmail.com>
2024-11-26 12:47:28 +0300

    Update smart-splits

--- lua/plugins/smart-splits.lua
+++ lua/plugins/smart-splits.lua
@@ -4,7 +4,35 @@ local M = {
 }
 
 M.config = function()
-	require("nvim-autopairs").setup()
+	vim.keymap.set("n", "<A-h>", require("smart-splits").resize_left)
+	vim.keymap.set("n", "<A-j>", require("smart-splits").resize_down)
+	vim.keymap.set("n", "<A-k>", require("smart-splits").resize_up)
+	vim.keymap.set("n", "<A-l>", require("smart-splits").resize_right)
+	-- moving between splits
+	vim.keymap.set("n", "<C-h>", require("smart-splits").move_cursor_left)
+	vim.keymap.set("n", "<C-j>", require("smart-splits").move_cursor_down)
+	vim.keymap.set("n", "<C-k>", require("smart-splits").move_cursor_up)
+	vim.keymap.set("n", "<C-l>", require("smart-splits").move_cursor_right)
+	vim.keymap.set("n", "<C-\\>", require("smart-splits").move_cursor_previous)
+	-- swapping buffers between windows
+	vim.keymap.set("n", "<leader><leader>h", require("smart-splits").swap_buf_left)
+	vim.keymap.set("n", "<leader><leader>j", require("smart-splits").swap_buf_down)
+	vim.keymap.set("n", "<leader><leader>k", require("smart-splits").swap_buf_up)
+	vim.keymap.set("n", "<leader><leader>l", require("smart-splits").swap_buf_right)
+
+	require("smart-splits").setup({
+		resize_mode = {
+			silent = true,
+			hooks = {
+				on_enter = function()
+					vim.notify("Entering resize mode")
+				end,
+				on_leave = function()
+					vim.notify("Exiting resize mode, bye")
+				end,
+			},
+		},
+	})
 end
 
 return M
