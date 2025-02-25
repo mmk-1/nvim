da4c86900fdd56347b1a75b926c5657f8804ede2
Mahdi Khosravi <mmk1776@gmail.com>
2025-02-25 22:03:02 +0300

    updates

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,35 +1,35 @@
 local M = {
-	"stevearc/conform.nvim",
-	opts = {},
+  "stevearc/conform.nvim",
+  opts = {},
 }
 
 function M.config()
-	require("conform").setup({
-		-- Define your formatters by filetype
-		formatters_by_ft = {
-			lua = { "stylua" },
-			nix = { "alejandra" },
-			javascript = { "prettier" },
-			typescript = { "prettier" },
-			jsx = { "prettier" },
-			cpp = { "clang_format" },
-			c = { "clang_format" },
-			h = { "clang_format" },
-		},
+  require("conform").setup({
+    -- Define your formatters by filetype
+    formatters_by_ft = {
+      lua = { "stylua" },
+      nix = { "alejandra" },
+      javascript = { "prettier" },
+      typescript = { "prettier" },
+      jsx = { "prettier" },
+      cpp = { "clang_format" },
+      c = { "clang_format" },
+      h = { "clang_format" },
+    },
 
-		format_on_save = {
-			lsp_fallback = true,
-			timeout_ms = 500,
-		},
+    format_on_save = {
+      lsp_fallback = true,
+      timeout_ms = 500,
+    },
 
-		-- Customize formatters
-		clang_format = {
-			formatters = {
-				"-style",
-				"{IndentWidth: 4}",
-			},
-		},
-	})
+    -- Customize formatters
+    clang_format = {
+      formatters = {
+        "-style",
+        "{IndentWidth: 4}",
+      },
+    },
+  })
 end
 
 return M
