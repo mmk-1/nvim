6c46178cd30d6067fac0d1e349316b3e689c8540
Mahdi Khosravi <mmk1776@gmail.com>
2025-02-27 16:09:42 +0300

    Added leap.nvim

--- lua/plugins/project.lua
+++ lua/plugins/project.lua
@@ -1,13 +1,13 @@
 local M = {
   "ahmedkhalf/project.nvim",
-  opts = {
-    manual_mode = true,
-  },
   event = "VeryLazy",
-  config = function(_, opts)
-    require("project_nvim").setup(opts)
-    require("telescope").load_extension("projects")
-  end,
 }
+
+function M.config()
+  require("project_nvim").setup({
+    manual_mode = true
+  })
+  require("telescope").load_extension("projects")
+end
 
 return M
