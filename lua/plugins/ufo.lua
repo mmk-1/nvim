08b021dc8bb9acb5a26077db0dd458ad98b9742b
Mahdi Khosravi <mmk1776@gmail.com>
2025-04-09 18:11:23 +0300

    Add neovim UFO for folding

--- lua/plugins/ufo.lua
+++ lua/plugins/ufo.lua
@@ -1,49 +1,27 @@
--- local M = {
---   "kevinhwang91/nvim-ufo",
---   dependencies = {
---     "kevinhwang91/promise-async",
---     "luukvbaal/statuscol.nvim",
---   },
--- }
---
--- function M.config()
---   local builtin = require "statuscol.builtin"
---   local cfg = {
---     setopt = true,
---     relculright = true,
---     segments = {
---       { text = { builtin.foldfunc, " " }, click = "v:lua.ScFa", hl = "Comment" },
---       { text = { "%s" }, click = "v:lua.ScSa" },
---       { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
---     },
---   }
---
---   require("statuscol").setup(cfg)
---
---   vim.o.foldcolumn = "1"
---   vim.o.foldlevel = 99
---   vim.o.foldlevelstart = 99
---   vim.o.foldenable = true
---
---   vim.keymap.set("n", "zR", require("ufo").openAllFolds)
---   vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
---
---   local capabilities = vim.lsp.protocol.make_client_capabilities()
---   capabilities.textDocument.foldingRange = {
---     dynamicRegistration = false,
---     lineFoldingOnly = true,
---   }
---   local language_servers = require("lspconfig").util.available_servers()
---   for _, ls in ipairs(language_servers) do
---     require("lspconfig")[ls].setup({
---       capabilities = capabilities,
---     })
---   end
---   require("ufo").setup()
--- end
-
 local M = {
-
+  "kevinhwang91/nvim-ufo",
+  dependencies = {
+    "kevinhwang91/promise-async",
+  },
 }
+
+-- Minimal config for UFO
+function M.config()
+  vim.o.foldcolumn = '1' -- '0' is not bad
+  vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
+  vim.o.foldlevelstart = 99
+  vim.o.foldenable = true
+
+  -- Using ufo provider need remap `zR` and `zM`.
+  vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+  vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+
+  -- Fold using treesitter as provider
+  require('ufo').setup({
+    provider_selector = function(bufnr, filetype, buftype)
+      return { 'treesitter', 'indent' }
+    end
+  })
+end
 
 return M
