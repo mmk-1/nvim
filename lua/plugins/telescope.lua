49bcc2d6025fde22ced3107d95fede1beea517ef
Mahdi Khosravi <mmk1776@gmail.com>
2025-04-09 23:22:07 +0300

    Minor refactorings

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,16 +1,16 @@
 local M = {
-	"nvim-telescope/telescope.nvim",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	config = true,
-	keys = {
-		{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
-		{ "<leader>fc", "<cmd>Telescope commands<cr>", desc = "Find files" },
-		{ "<leader>fk", "<cmd>Telescope keymaps<cr>", desc = "Find files" },
-		{ "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Find files" },
-		{ "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Grep (Project dir)" },
-		{ "<leader>pp", "<cmd>Telescope projects<cr>", desc = "Projects" },
-		{ "<leader>bb", "<cmd>Telescope buffers<cr>", desc = "Find buffers" },
-	},
+  "nvim-telescope/telescope.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = true,
+  keys = {
+    { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
+    { "<leader>fc", "<cmd>Telescope commands<cr>",   desc = "Find commands" },
+    { "<leader>fk", "<cmd>Telescope keymaps<cr>",    desc = "Find keymaps" },
+    { "<leader>fh", "<cmd>Telescope help_tags<cr>",  desc = "Find tags" },
+    { "<leader>fg", "<cmd>Telescope live_grep<cr>",  desc = "Grep (Project dir)" },
+    { "<leader>pp", "<cmd>Telescope projects<cr>",   desc = "Projects" },
+    { "<leader>bb", "<cmd>Telescope buffers<cr>",    desc = "Find buffers" },
+  },
 }
 
 return M
