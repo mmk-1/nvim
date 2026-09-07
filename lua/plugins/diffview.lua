c68f71491b9c8b0c792965b3f1aca0a5587859fe
Mahdi Khosravi <87431684+mmk-1@users.noreply.github.com>
2026-09-07 23:06:00 +0300

    Adjust git related plugins

---
+++ lua/plugins/diffview.lua
@@ -0,0 +1,29 @@
+return {
+  "sindrets/diffview.nvim",
+  cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewFileHistory" },
+  keys = {
+    { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Diffview" },
+  },
+  opts = {
+    file_panel = {
+      win_config = {
+        width = 24,
+        win_opts = {
+          signcolumn = "no", -- to reduce teh width for file browser
+          statuscolumn = "",
+        },
+      },
+    },
+    keymaps = {
+      view = {
+        { "n", "q", "<cmd>DiffviewClose<cr>", { desc = "Close Diffview" } },
+      },
+      file_panel = {
+        { "n", "q", "<cmd>DiffviewClose<cr>", { desc = "Close Diffview" } },
+      },
+      file_history_panel = {
+        { "n", "q", "<cmd>DiffviewClose<cr>", { desc = "Close Diffview" } },
+      },
+    },
+  },
+}
--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -3,13 +3,11 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
     "sindrets/diffview.nvim",
-    "folke/snacks.nvim",
   },
-  cmd = { "Neogit", "DiffviewOpen", "DiffviewClose", "DiffviewFileHistory" },
+  cmd = { "Neogit" },
   keys = {
     { "<leader>gg", "<cmd>Neogit<cr>", desc = "Neogit" },
     { "<leader>gc", "<cmd>Neogit commit<cr>", desc = "Commit" },
-    { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Diffview" },
   },
   opts = {
     integrations = {
